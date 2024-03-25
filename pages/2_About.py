2f182e8b921579dad2dbcf1e032d6ee5342e7e1c
Thiago Oliveira Borges <86489192+ThiagoBorges81@users.noreply.github.com>
2024-03-25 11:54:45 -0300

    Update and rename 3_About.py to 2_About.py

--- pages/2_About.py
+++ pages/2_About.py
@@ -5,4 +5,15 @@ st.write(
     "Discover relevant research effortlessly with our app, harnessing the power of PubMed. Simply input your search terms, and we'll retrieve references matching your criteria. Filter results by keywords in titles, abstracts, or keywords. Delve deeper with Word Cloud visualizations, aiding swift analysis of selected abstracts. Streamline your research journey today."
 )
 st.write('')
+st.write('')
+st.subheader(
+    "How to use this app")
+st.markdown(
+    """
+    - 1st - Click on the 'Searcher' link on the sidebar;  
+    - 2nd - Insert the keywords of interest in the search box, separated by spaces;  
+     
+    
+    """
+    "")
 st.write('app version: 0.0.1')
